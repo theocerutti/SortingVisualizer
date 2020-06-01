afca26238320f2a9fcba695e5fa929e144fb70f4
theo cerutti <theo.cerutti@epitech.eu>
2020-06-01 13:24:33 +0200

    Add cmake and sfml

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2020
+** $HEADER_FILENAME
+** File description:
+**
+*/
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Draw the sprite
+
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
